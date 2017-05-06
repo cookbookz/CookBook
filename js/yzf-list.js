aa9feccf8a44b5e6acbcf1a6887f30ce04e788ff
yangzefeng520 <1213128156@qq.com>
2017-05-07 02:48:17 +0800

    yzf

--- js/yzf-list.js
+++ js/yzf-list.js
@@ -6,4 +6,32 @@ $(document).ready(function(){
 			$('.yzf-chanpin').eq(index).toggleClass('active');
 		})
 	})
+	
+	
+//	清空
+	$('.yzf-del').on('touchstart',function(){
+		$('ul').css('display','none');
+	})
+	
+//	点击
+	$('.yzf-center > span:first-child').on('touchstart',function(){
+		$(this).css({
+			"background":"#ffdc3b",
+			"color":"#fff"
+		});
+		$('.yzf-center > span:nth-child(2)').css({
+			'background':'#fff',
+			'color':'#ffdc3b'
+		});
+	})
+	$('.yzf-center > span:nth-child(2)').on('touchstart',function(){
+		$(this).css({
+			"background":"#ffdc3b",
+			"color":"#fff"
+		});
+		$('.yzf-center > span:nth-child(1)').css({
+			'background':'#fff',
+			'color':'#ffdc3b'
+		});
+	})
 })
