ea069c66ce1fdc489f1fca51b3c5c971a81e4712
yangzefeng520 <1213128156@qq.com>
2017-05-07 23:05:15 +0800

    as

--- js/yzf-friend.js
+++ js/yzf-friend.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	$('.bb > li').each(function(index,value){
 		$(this).on('touchstart',function(){
 			$(this).on('touchmove',function(){
-				$(this).css('transform','translateX(-0.5rem)');
-				$('.guanzhu').eq(index).css('display','none');
-				$('.del').eq(index).css('display','block');
+				$(this).toggleClass('move');
+				$('.guanzhu').eq(index).toggleClass('active');
+				$('.del').eq(index).toggleClass('active2');
 				$('.del img:first-child').on('touchstart',function(){
 					$('.bb > li').eq(index).prependTo('.bb');
-					$('.bb > li').eq(index).css('transform','translateX(0)');
-					$('.guanzhu').eq(index).css('display','block');
-					$('.del').eq(index).css('display','none');
+					// $('.bb > li').eq(index).css('transform','translateX(0)');
+					// $('.guanzhu').eq(index).css('display','block');
+					// $('.del').eq(index).css('display','none');
 				})
 				$('.del img:nth-child(2)').on('touchstart',function(){
 					$('.bb > li').eq(index).css('display','none');
@@ -17,10 +17,18 @@ $(document).ready(function(){
 			})
 		})
 	})
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+//列表
+
 	$('.yzf-center > span:first-child').on('touchstart',function(){
 		$(this).css({
 			"background":"#ffdc3b",
