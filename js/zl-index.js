98aa452e0123600e6fd7f78e102e90d5b2ec8a08
bloodkey <3028336816@qq.com>
2017-05-08 08:47:38 +0800

    zl

--- js/zl-index.js
+++ js/zl-index.js
@@ -62,6 +62,22 @@ $(function(){
 			$('.zl-zao').off().css('height','0')
 		// })
 	})
+	$('.zl-meishi').on('touchstart',function(){
+		var zl=document.cookie;
+		if(zl=='denglu'){
+			location.href='yzf-meishi.html';
+		}else{
+			location.href='ly3.html';
+		}
+	})
+	$('.zl-chuancai').on('touchstart',function(){
+		var zl=document.cookie;
+		if(zl=='denglu'){
+			location.href='zl-chuancai.html';
+		}else{
+			location.href='ly3.html';
+		}
+	})
 // 搜索框
 	// $('.zl-search >input').on('focus',function(){
 	// 	// location.href=''
@@ -72,15 +88,16 @@ $(function(){
 		location.href='zl-sousuo.html'
 	})
 // 侧滑
-	// $('.zl-god').swipeRight(function(){
-	// 	console.log('you')
-	// 	$('body').css('transform','rotateY(45deg)')
-	// })
-	// var body=document.querySelector('body');
-       
+     
  	$('.zl-menu').on('touchstart',function(){
- 		$('.zl-you').css({'left':'5.8rem','transform':'rotateY(-30deg)'})
- 		$('.zl-zuo').css('left','0')
+ 		var zl=document.cookie;
+		if(zl=='denglu'){
+	 		$('.zl-you').css({'left':'5.8rem','transform':'rotateY(-30deg)'})
+	 		$('.zl-zuo').css('left','0')
+			
+		}else{
+			location.href='ly3.html';
+		}  
  	})
 	var hammertime = new Hammer(document.querySelector(".zl-zuo"));
 	//为该dom元素指定触屏移动事件
