f950cbe16facb3d0e68f24006c939a738a76999b
yangzefeng520 <1213128156@qq.com>
2017-05-08 21:21:31 +0800

    lianjie

--- js/yzf-pinglun.js
+++ js/yzf-pinglun.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 	$('textarea').on('touchstart',function(){
 		$(this).html("");
 	})
+	
+	    //返回
+    $('.yzf-nav img:first-child').on('touchstart',function(){
+        history.back(0);
+        // console.log(1);
+    })
 })
